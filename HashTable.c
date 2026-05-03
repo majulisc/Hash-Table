f05a53cd19ccf2185d4b90bf36002cf3233906d0
Maria Júlia S Carvalho <mariajuliasouzadecarvalho1@gmail.com>
2026-05-03 19:48:29 -0300

    Implemntação do procedimento inserir e da funcao busca

--- HashTable.c
+++ HashTable.c
@@ -2,27 +2,49 @@
 #include <stdlib.h>
 // 2 * 15 (quantidade de elementos)== 30 -> 29 (primo mais proximo)
 // meu conjunto de dados apresenta apenas numeros inteiros positivos e nnega o 0
-#define TAM  29
+#define TAM  29 
 
-void inicializarTabela (int tabela[]){
+void inicializarTabela (int tabelaHash[]){
     int i;
     for( i= 0; i < TAM; i++){
-        tabela[i] = 0; // acesso a tabela na psoicao i e informo que ela ira receber o valor 0--> inicializo com 0 
+        tabelaHash[i] = 0; // acesso a tabela na posicao i e informo que ela ira receber o valor 0 passando por todos os indices--> inicializo com 0 
 
     }
 }
 
-int funcaoHash(int chave){ // a funcao hash vai calcular o indice com base no valor da chamee
+int funcaoHash(int chave){ // a funcao hash vai calcular o indice com base no valor da chave
     return chave % TAM; // retorno do resto
+} // a funcaoHash é a funcao de espalhamento
+
+void inserir(int chave, int tabelaHash[]){ //funcao que descobre o indice e armazena a chave na tabela Hash
+    int indice = funcaoHash(chave); //inserir no indice o valor da chave no indice correto
+    
+    while(tabelaHash[indice] != 0){ // caso seja != 0 isso indica que esse indice ja esta oculpado
+        indice = funcaoHash (indice + 1); //pula pra o proximo indice TRATAMENTO E COLISAO
+        //while termina quando achamos um indice vazio e armazenamos a chave nele!
+    }
 }
 
-void inserir(int chave, int tabela[]){ //funcao que descobre o indice e armazena a chave na tabela Hash
+int busca (int tabelaHash[], int chave){
     int indice = funcaoHash(chave);
-    while(tabela[indice] != 0){ // caso seja != 0 isso indica que esse indice ja esta oculpado
-        indice = funcaoHash (indice + 1); //pula pra o proximo indice
+    while(tabelaHash[indice] != 0){
+        if(tabelaHash[indice] == chave){
+            return tabelaHash [indice];
+
+        }
+        else {
+            indice = funcaoHash (indice + 1);
+        }
+    }
+}
+
+void imprimir (tabelaHash[]){
+    int i;
+    for (i = 0; i < TAM; i++){
+        
     }
 }
 
 int main (){
-    int VetordeArmazenamento [TAM];
+    int tabela [TAM]; //o vetor de armazenamento é a nossa tabela hash e ela inicializa VAZIA 
 }
