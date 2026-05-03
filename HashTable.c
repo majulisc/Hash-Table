f8e3740fa52f0755f7f091b64a6a71bc172dc276
Maria Júlia S Carvalho <mariajuliasouzadecarvalho1@gmail.com>
2026-05-03 20:59:31 -0300

    correção de erros de sintaxe

--- HashTable.c
+++ HashTable.c
@@ -12,7 +12,7 @@ void inicializarTabela (int tabelaHash[]){
     }
 }
 
-    int funcaoHash(int chave){ // a funcao hash vai calcular o indice com base no valor da chave
+int funcaoHash(int chave){ // a funcao hash vai calcular o indice com base no valor da chave
     return chave % TAM; // retorno do resto
 } // a funcaoHash é a funcao de espalhamento
 
@@ -23,6 +23,7 @@ void inserir(int chave, int tabelaHash[]){ //funcao que descobre o indice e arma
         indice = funcaoHash (indice + 1); //pula pra o proximo indice TRATAMENTO E COLISAO
         //while termina quando achamos um indice vazio e armazenamos a chave nele!
     }
+    tabelaHash[indice] = chave;
 }
 
 int busca (int tabelaHash[], int chave){
@@ -35,46 +36,52 @@ int busca (int tabelaHash[], int chave){
         else {
             indice = funcaoHash (indice + 1);
         }
-        return 0;
+        
     }
+    return 0;
 }
 
 
-void imprimir (tabelaHash[]){
+void imprimir (int tabelaHash[]){
     int i;
     for (i = 0; i < TAM; i++){
         printf ("%d = %d\n", i, tabelaHash[i]);
 
     }
+}
 
 int main (){
     int tabela [TAM], opcao, retornoDaFuncaoBusca; //o vetor de armazenamento é a nossa tabela hash e ela inicializa VAZIA 
     do{
             printf("\n\t0 - Sair\n\t1 - Inserir\n\t2 - Buscar\n\t3 -Imprimir\n");
-            scanf(%d, & opcao);
+            scanf("%d", & opcao);
     switch(opcao){
     
     case 1:
+        printf("Qual valor deseja inserir");
+        scanf("%d", &valor);
+        inserir(valor, tabela);
         break;
     case 2:
         printf("Qual valor deseja buscar");
         scanf("%d", & valor);
-        retornoDaFuncaoBusca = busca(tabelaHash, valor);
-        if(retorno !=0){
-            printf("Valor encontrado %d!\n",retonoDaFuncaoBusca)
+        retornoDaFuncaoBusca = busca(tabela, valor);
+        if(retornoDaFuncaoBusca !=0){
+            printf("Valor encontrado %d!\n", retornoDaFuncaoBusca)
         }
         else{
             printf(" valor nao encontrado");
         }
         break;
     case 3:
-        imprimir(tabelaHash);
+        imprimir(tabela);
         break;
     default:
-        printf("OPÇAO INVALIDA!")     
+        printf("OPÇAO INVALIDA!");     
     }
-    }  while (opcao != 0);
-
-
+}
+while (opcao != 0);
+return 0;
+     
 
 }
